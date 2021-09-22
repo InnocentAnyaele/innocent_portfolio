1e2d8062a7ebac1cc316865bdfd8aca780e26198
InnocentAnyaele <luisnerma545@gmail.com>
2021-09-22 10:46:00 +0000

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,64 +23,20 @@ function Header() {
 				/>
 
 				<div>
-					<span className='name' style={{ fontSize: '25px', color: 'blue' }}>
+					<span className='name' style={{ fontSize: '27px', color: 'blue' }}>
 						Innocent Anyaele
 					</span>
 					<br></br>
-					<span className='field' style={{ fontSize: '15px', color: 'blue' }}>
+					<span className='field' style={{ fontSize: '17px', color: 'blue' }}>
 						Developer || Data Scientist
 					</span>
 					<br></br>
 					<br></br>
-
-					<a
-						href='https://drive.google.com/file/d/1bq1y49KFBOjl4ra0z5IZd0gmrdkbDn0V/view?usp=sharing'
-						download='https://drive.google.com/file/d/1bq1y49KFBOjl4ra0z5IZd0gmrdkbDn0V/view?usp=sharing'>
-						<Button className='m-3' color='primary'>
-							Get CV
-						</Button>
-					</a>
-				</div>
-			</div>
-
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'row',
-					minHeight: '100vh',
-					marginTop: '50px',
-					marginLeft: '50px',
-					marginRight: '50px',
-					justifyContent: 'space-evenly',
-					flexWrap: 'wrap',
-				}}>
-				<div>
-					<img
-						className='img-fluid'
-						src={profile}
-						alt='profile'
-						style={{ width: '500', height: '600', borderRadius: '50%' }}
-					/>
-				</div>
-				<div>
-					<div
-						className='container-fluid'
-						style={{ height: '100vh', padding: '50px', margin: '10px' }}>
-						<article className='about-message message is-link mx-auto p-3 img-fluid'>
-							<div className='message-body text-justify'>
-								A passionate Nigerian developer currently pursuing a Bachelor of
-								Science Degree in Information Technology at DataLink Institute
-								of Business and Technology. My primary languages include Python
-								and Javascript although conversant in Java, C++ and C#. I'm
-								experienced in React and Django but looking forward to work with
-								more frameworks. I have experience in machine learning from
-								various internships and hope to build a fruitful career in data
-								science. You can contact me at <br></br>
-								<strong>innocentanyaele2000@gmail.com </strong> <br></br>
-								<strong>+233 55 718 7667</strong>
-								<br></br>
-								Or use any of these socials
-								<ul
+					<span className='field' style={{ fontSize: '15px', color: 'blue' }}>
+						innocentanyaele2000@gmail.com <br></br> +233557187667
+						</span>
+						<br></br>
+					<ul
 									style={{ listStyle: 'none', display: 'inline-flex' }}
 									className='m-3'>
 									<li>
@@ -95,46 +51,63 @@ function Header() {
 										</a>
 									</li>
 								</ul>
-							</div>
-						</article>
-					</div>
+					<br></br>
+
+					<a
+						href='https://drive.google.com/file/d/1bq1y49KFBOjl4ra0z5IZd0gmrdkbDn0V/view?usp=sharing'
+						download='https://drive.google.com/file/d/1bq1y49KFBOjl4ra0z5IZd0gmrdkbDn0V/view?usp=sharing'>
+						<Button className='m-3' color='primary'>
+							Get CV
+						</Button>
+					</a>
 				</div>
 			</div>
 
-			{/* <div style={{ height: '100vh', padding: '50px' }}>
-				<article
-					className='about-message message is-link mx-auto p-3 img-fluid'
-					style={{ width: '500px' }}>
-					<div className='message-body text-justify'>
-						A passionate Nigerian developer currently pursuing a Bachelor of
-						Science Degree in Information Technology at DataLink Institute of
-						Business and Technology. My primary languages include Python and
-						Javascript although conversant in Java, C++ and C#. I'm experienced
-						in React and Django but looking forward to work with more
-						frameworks. I have experience in machine learning from various
-						internships and hope to build a fruitful career in data science. You
-						can contact me at <br></br>
-						<strong>innocentanyaele2000@gmail.com </strong> <br></br>
-						<strong>+233 55 718 7667</strong>
-						<br></br>
-						Or use any of these socials
-						<ul
-							style={{ listStyle: 'none', display: 'inline-flex' }}
-							className='m-3'>
-							<li>
-								<a href='https://github.com/InnocentAnyaele'>
-									<GitHubIcon style={{ color: 'blue' }} />
-								</a>
-							</li>{' '}
-							<br></br>
-							<li>
-								<a href='https://gh.linkedin.com/in/innocent-anyaele-398825165'>
-									<LinkedInIcon style={{ color: 'blue' }} />
-								</a>
-							</li>
-						</ul>
+			<div
+						className='container-fluid'
+						style={{ height: 'auto', padding: '50px' }}>
+						<article className='about-message message is-link mx-auto p-3 img-fluid'>
+							<div className='message-body text-center'>
+							“Man is condemned to be free” – Jean-Paul Sartre
+							</div>
+						</article>
 					</div>
-				</article>
+
+					<div className='p-5 d-flex justify-content-center align-items-center w-100'>
+						<div className='w-100' style={{maxWidth: '700px'}}>
+						<img
+						className='img-fluid w-100 h-100'
+						src={profile}
+						alt='profile'
+						style={{borderRadius: '50%' }}
+					/>
+						</div>
+					</div>
+
+			{/* <div
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+					maxHeight: '700px',
+					maxWidth: '700px',
+					width: '100%',
+					height: '100%',
+					marginTop: '50px',
+					marginLeft: '50px',
+					marginRight: '50px',
+					justifyContent: 'center',
+					flexWrap: 'wrap',
+				}}>
+				<div>
+					<img
+						className='img-fluid'
+						src={profile}
+						alt='profile'
+						style={{ width: '500', height: '600', borderRadius: '50%' }}
+					/>
+				</div>
+				<div>
+				</div>
 			</div> */}
 		</div>
 	);
